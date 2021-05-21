df94e97bd093e3187d2ca19393df9b1b15255819
Fl_GUI <flor.guilini@hotmail.com>
2021-05-21 20:34:38 +0200

    disabled onmessage because conflict

--- main.js
+++ main.js
@@ -6,20 +6,20 @@ const env = require('dotenv').config({
 const srb2kartinfo = require("./srb2kartInfoParser/srb2kartserverinfo.js").getSrb2Info;
 
 const client = new Discord.Client();
-let i = 0;
 
 const updateStatus = () => {
   srb2kartinfo(env.SERVER, env.PORT, (server)=>{
     const stat = `${server.numberofplayers} players race`
     client.user.setActivity(stat, {type: 'WATCHING'})
-      .catch(console.log);
+      .catch(console.error);
   }, ()=>{});
 }
 
 client.on('message', (message) => {
+  return
   if(message.author.bot)
     return
-  if(message.content.trim() === "!players")
+  if(message.content.trim() === "!players") {
     srb2kartinfo(env.SERVER, env.PORT, () => {}, (plrs)=>{
       const players = plrs.playerinfo.map((plrinfo) => {
         return plrinfo.name
@@ -34,10 +34,12 @@ client.on('message', (message) => {
         names = players.join(", ");
         response = `${players.join(', ')} and ${last} are online`;
       }
-      message.channel.send(response);
+      message.channel.send(response)
+        .then(message => console.log(`sent a message at ${message.createdAt}`))
+        .catch(console.error);
     });
-
-});
+  }
+})
 
 client.on('ready', () => {
   setInterval(updateStatus, env.INTEVAL);
