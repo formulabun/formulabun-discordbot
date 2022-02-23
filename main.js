1b5b964e2342cc1e6b00ad7f3a8ed5587ceacbd9
Fl_GUI <flor.guilini@hotmail.com>
2022-02-23 12:52:30 +0100

    data update to main

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 import FormulaBunBot from './client.js';
 import {config} from 'dotenv';
 const env = config().parsed;
+import {getSrb2Info} from 'srb2kartjs';
 
 
 let client = new FormulaBunBot({intents: []});
 client.login(env.DISCORD_TOKEN);
 
-setInterval(() => client.updateData(), parseInt(env.INTERVAL));
+setInterval(() => {
+  getSrb2Info(
+    env.SERVER, env.PORT,
+    info => {client.serverinfo = info},
+    info => {client.playerinfo = info},
+    error => {client.error = error}
+  )
+}, parseInt(env.INTERVAL));
+
+process.on("exit", () => client.detroy());
