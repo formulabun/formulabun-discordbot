dbef54cbe36dab8a1eb7d2ad8e1fc341d0b32935
Fl_GUI <flor.guilini@hotmail.com>
2022-05-15 10:38:55 +0200

    revert fbun references

--- client/commands.js
+++ client/commands.js
@@ -5,38 +5,46 @@ import { MessageEmbed } from "discord.js";
 
 import maplookup from "./mapcommand.js";
 import players from "./playerscommand.js";
-
-const join = {
+const joinfbun = {
   descr: "explain how to join this epic server",
   respond: async () => {
     return {
       embed: [
         new MessageEmbed()
-          .setColor("#ffcece")
-          .setTitle("Joining this server")
-          .setURL(`http://${SERVER}`)
-          .addField(
-            "Option one:",
-            `type \`${SERVER}\` into the **join a game** field`
-          )
-          .addField(
-            "Option two:",
-            `create a \`kartexec.cfg\` file in your srb2kart folder and add the following line:
+        .setColor("#ffcece")
+        .setTitle("Joining Formula bun")
+        .setURL(`http://${SERVER}`)
+        .setImage("http://formulabun.club/fastbun.png")
+        .addField(
+          "Option one:",
+          `type \`${SERVER}\` into the **join a game** field`
+        )
+        .addField(
+          "Option two:",
+          `create a \`kartexec.cfg\` file in your srb2kart folder and add the following line:
 > alias joinserver "connect ${SERVER}"
 and join the server by opening the console with using the \\\` key and enter \`joinserver\`
-`
-          )
-          .addField(
-            "Option three:",
-            `Copy paste the following url into your browser (when supported on your device) \`srb2kart://ip/${SERVER}\``
-          ),
+                                            `
+        )
+        .addField(
+          "Option three:",
+          `Copy paste the following url into your browser (when supported on your device) \`srb2kart://ip/${SERVER}\``
+        ),
       ],
     };
   },
 };
 
+const prayers = {
+    descr: "prayers",
+    respond: () => {
+          return { content: ":pray:" };
+        },
+};
+
 export default {
-  players,
-  join,
-  maplookup,
+    players,
+    joinfbun,
+    prayers,
+    maplookup,
 };
